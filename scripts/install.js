549b848c037070f863c4de1e37461c1c3b282833
alirdn <alireza.dabirinejad@live.com>
2018-07-13 18:57:15 +0430

    Reformat file.

--- scripts/install.js
+++ scripts/install.js
@@ -2,11 +2,11 @@ var os = require('os');
 var cp = require('child_process');
 
 if (os.platform() === 'win32') {
-    var result = cp.spawnSync('npm.cmd', ['run', 'native-build'], {
+    var result = cp.spawnSync('npm.cmd', ['run', 'install-native'], {
         stdio: 'inherit'
     });
 
-    if(result.status !== 0 || result.stderr !== null || result.signal !== null) {
+    if (result.status !== 0 || result.stderr !== null || result.signal !== null) {
         throw new Error('An issue occured during installing native module.');
     }
 }
