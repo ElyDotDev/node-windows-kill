bb06d1bf9080defee4ce72d1f37703f458225512
alirdn <alireza.dabirinejad@live.com>
2018-07-15 03:29:15 +0430

    Downgrade ava from 1.0.0beta.3 to 0.25.0

--- test/functionality-general.test.js
+++ test/functionality-general.test.js
@@ -7,44 +7,61 @@ var WindowsKill = require('..')
 var windowsKill = WindowsKill();
 
 test('sending SIGBREAK with invalid pid using returned function should throw exception', function (t) {
-    t.throws(function () { windowsKill('INVALID', 'SIGBREAK'); }, { instanceOf: TypeError, message: 'invalid pid.' }, 'Exception error is instance of TypeError for pid="INVALID"');
-    t.throws(function () { windowsKill('10.2', 'SIGBREAK'); }, { instanceOf: TypeError, message: 'invalid pid.' }, 'Exception error is instance of TypeError for pid="10.2"');
-    t.throws(function () { windowsKill(10.2, 'SIGBREAK'); }, { instanceOf: TypeError, message: 'invalid pid.' }, 'Exception error is instance of TypeError for pid=10.2');
+    var error = t.throws(function () { windowsKill('INVALID', 'SIGBREAK'); }, TypeError, 'Exception error is instance of TypeError for pid="INVALID"');
+    t.is(error.message, 'invalid pid.');
+    error = t.throws(function () { windowsKill('10.2', 'SIGBREAK'); }, TypeError, 'Exception error is instance of TypeError for pid="10.2"');
+    t.is(error.message, 'invalid pid.');
+    error = t.throws(function () { windowsKill(10.2, 'SIGBREAK'); }, TypeError, 'Exception error is instance of TypeError for pid=10.2');
+    t.is(error.message, 'invalid pid.');
     t.pass();
 });
 
+
 test('sending SIGINT with invalid pid using returned function should throw exception', function (t) {
-    t.throws(function () { windowsKill('INVALID', 'SIGINT'); }, { instanceOf: TypeError, message: 'invalid pid.' }, 'Exception error is instance of TypeError for pid="INVALID"');
-    t.throws(function () { windowsKill('10.2', 'SIGINT'); }, { instanceOf: TypeError, message: 'invalid pid.' }, 'Exception error is instance of TypeError for pid="10.2"');
-    t.throws(function () { windowsKill(10.2, 'SIGINT'); }, { instanceOf: TypeError, message: 'invalid pid.' }, 'Exception error is instance of TypeError for pid=10.2');
+    var error = t.throws(function () { windowsKill('INVALID', 'SIGINT'); }, TypeError, 'Exception error is instance of TypeError for pid="INVALID"');
+    t.is(error.message, 'invalid pid.');
+    error = t.throws(function () { windowsKill('10.2', 'SIGINT'); }, TypeError, 'Exception error is instance of TypeError for pid="10.2"');
+    t.is(error.message, 'invalid pid.');
+    error = t.throws(function () { windowsKill(10.2, 'SIGINT'); }, TypeError, 'Exception error is instance of TypeError for pid=10.2');
+    t.is(error.message, 'invalid pid.');
     t.pass();
 });
 
 test('sending SIGBREAK with invalid pid using process.kill function should throw exception', function (t) {
-    t.throws(function () { process.kill('INVALID', 'SIGBREAK'); }, { instanceOf: TypeError, message: 'invalid pid.' }, 'Exception error is instance of TypeError for pid="INVALID"');
-    t.throws(function () { process.kill('10.2', 'SIGBREAK'); }, { instanceOf: TypeError, message: 'invalid pid.' }, 'Exception error is instance of TypeError for pid="10.2"');
-    t.throws(function () { process.kill(10.2, 'SIGBREAK'); }, { instanceOf: TypeError, message: 'invalid pid.' }, 'Exception error is instance of TypeError for pid=10.2');
+    var error = t.throws(function () { process.kill('INVALID', 'SIGBREAK'); }, TypeError, 'Exception error is instance of TypeError for pid="INVALID"');
+    t.is(error.message, 'invalid pid.');
+    error = t.throws(function () { process.kill('10.2', 'SIGBREAK'); }, TypeError, 'Exception error is instance of TypeError for pid="10.2"');
+    t.is(error.message, 'invalid pid.');
+    error = t.throws(function () { process.kill(10.2, 'SIGBREAK'); }, TypeError, 'Exception error is instance of TypeError for pid=10.2');
+    t.is(error.message, 'invalid pid.');
     t.pass();
 });
 
 test('sending SIGINT with invalid pid using process.kill function should throw exception', function (t) {
-    t.throws(function () { process.kill('INVALID', 'SIGINT'); }, { instanceOf: TypeError, message: 'invalid pid.' }, 'Exception error is instance of TypeError for pid="INVALID"');
-    t.throws(function () { process.kill('10.2', 'SIGINT'); }, { instanceOf: TypeError, message: 'invalid pid.' }, 'Exception error is instance of TypeError for pid="10.2"');
-    t.throws(function () { process.kill(10.2, 'SIGINT'); }, { instanceOf: TypeError, message: 'invalid pid.' }, 'Exception error is instance of TypeError for pid=10.2');
+    var error = t.throws(function () { process.kill('INVALID', 'SIGINT'); }, TypeError, 'Exception error is instance of TypeError for pid="INVALID"');
+    t.is(error.message, 'invalid pid.');
+    error = t.throws(function () { process.kill('10.2', 'SIGINT'); }, TypeError, 'Exception error is instance of TypeError for pid="10.2"');
+    t.is(error.message, 'invalid pid.');
+    error = t.throws(function () { process.kill(10.2, 'SIGINT'); }, TypeError, 'Exception error is instance of TypeError for pid=10.2');
+    t.is(error.message, 'invalid pid.');
     t.pass();
 });
 
 test('sending SIGBREAK signal to not exsisting should throw exception', function (t) {
-    t.throws(function () { windowsKill(-1, 'SIGBREAK'); }, { instanceOf: Error, message: 'windows-kill ESRCH' }, 'Exception error is instance of Error');
+    var error = t.throws(function () { windowsKill(-1, 'SIGBREAK'); }, Error, 'Exception error is instance of Error');
+    t.is(error.message, 'windows-kill ESRCH');
     t.pass();
 });
 
 test('sending SIGINT signal to not exsisting should throw exception', function (t) {
-    t.throws(function () { windowsKill(-1, 'SIGINT'); }, { instanceOf: Error, message: 'windows-kill ESRCH' }, 'Exception error is instance of Error');
+    var error = t.throws(function () { windowsKill(-1, 'SIGINT'); }, Error, 'Exception error is instance of Error');
+    t.is(error.message, 'windows-kill ESRCH');
     t.pass();
 });
 
 test('should pass to node\'s process.kill if signal type (here 0) not supported', function (t) {
-    t.throws(function () { windowsKill(-1, 0); }, { instanceOf: Error, message: 'kill ESRCH' }, 'Exception error is instance of Error');
+    var error = t.throws(function () { windowsKill(-1, 0); }, Error, 'Exception error is instance of Error');
+    t.is(error.message, 'kill ESRCH');
     t.pass();
 });
+
